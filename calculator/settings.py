934a71401b6d7ecf7a07e2fede11b8c5e9f2bd29
Jason Bongiovanni <jpbon@protonmail.com>
2022-05-20 14:41:01 -0400

    first commit

--- calculator/settings.py
+++ calculator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-524w^c(ge2zc$dz9fo96)4)e5z+$qab@q&7-2(@u!x0di&zz@@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -22,19 +22,111 @@ def searchbar(request):
 def calculation(request):
     if request.method=="POST":
         values=request.POST['values'] #the input is gathered as a whole string
+        print(values)
+        if "(" in values:
+        # Start if there are Parenthesis
+            parentheses = re.findall(r"([(][^)]*[)])", values) #isolates the parenthesis
+            print(parentheses)
+            for par in parentheses:
+                
+                
+                par = par[1:-1] #removes parenthesis form expression
+                pars = re.findall(r"(\d+)",par) #separates digits from operators
+                operators=['+','x','÷','-','.','%']
+                opr = []
+                
+                for v in par:            
+                    for o in operators:
+                        if v==o:
+                            opr.append(o)
+                
+                for o in opr:
+                    if o=='.':
+                        i=opr.index(o)
+                        res=pars[i]+"."+pars[i+1]
+                        pars.remove(pars[i+1])
+                        opr.remove(opr[i])
+                        pars[i]=res
+
+                
+                for o in opr:
+                    if o=='%':
+                        i=opr.index(o)
+                        res=(float(pars[i])/100)*float(pars[i+1])
+                        pars.remove(pars[i+1])
+                        opr.remove(opr[i])
+                        pars[i]=res
+                        
+                        
+                for o in opr:
+                    if o=='÷':
+                        i=opr.index(o)
+                        res=float(pars[i])/float(pars[i+1])
+                        pars.remove(pars[i+1])
+                        opr.remove(opr[i])
+                        pars[i]=str(res)
+                        
+                        
+                for o in opr:
+                    if o=='x':
+                        i=opr.index(o)
+                        res=float(pars[i])*float(pars[i+1])
+                        pars.remove(pars[i+1])
+                        opr.remove(opr[i])
+                        pars[i]=str(res)
+                        
+                        
+                for o in opr:
+                    if o=='+':
+                        i=opr.index(o)
+                        res=float(pars[i])+float(pars[i+1])
+                        pars.remove(pars[i+1])
+                        opr.remove(opr[i])
+                        pars[i]=str(res)
+                        
+
+                    if o=='-':
+                        i=opr.index(o)
+                        res=float(pars[i])-float(pars[i+1])
+                        pars.remove(pars[i+1])
+                        opr.remove(opr[i])
+                        pars[i]=str(res)
+                        
+                
+                if len(opr)!=0:
+                    if opr[0]=='÷':
+                        result = float(pars[0])/float(pars[1])
+                    elif opr[0]=='x':
+                        result = float(pars[0])*float(pars[1])
+                    elif opr[0]=='+':
+                        result = float(pars[0])+float(pars[1])
+                    else :
+                        result = float(pars[0])-float(pars[1])
+                else:
+                    result = float(pars[0])
+                
+                result = str(result)
+                
+                #replace parentheses oof POST values with parentheses results
+                print(result)
+                values = values.replace(par, result)
+                print(values)
         
-        vals=re.findall(r"(\d+)",values) #REGEX is then used to isolate the integers
+        vals=re.findall(r"(\d+)",values) #REGEX is then used to isolate the integers and puts them in an list of strings
+        
         operators=['+','x','÷','-','.','%']
         opr=[]
         #the for loops below are used to loop through the list of possible operators
         #and the value string. Once a match is found, the operator is identified and then appened
         #into the opr list.
         #Based on what the opr is, the logic changes
+        
         for v in values:            
             for o in operators:
                 if v==o:
                     opr.append(o)
-        
+        # print(vals)
+        # print(opr)
         for o in opr:
             if o=='.':
                 i=opr.index(o)
@@ -42,7 +134,14 @@ def calculation(request):
                 vals.remove(vals[i+1])
                 opr.remove(opr[i])
                 vals[i]=res
-                
+                print(vals)
+                print(opr)
+        
+        for o in opr:
+            if o=='(':
+                i=opr.index(o)
+                res="(" + vals[i]
+        
         for o in opr:
             if o=='%':
                 i=opr.index(o)
@@ -50,6 +149,8 @@ def calculation(request):
                 vals.remove(vals[i+1])
                 opr.remove(opr[i])
                 vals[i]=res
+                # print(vals)
+                # print(opr)
                 
         for o in opr:
             if o=='÷':
@@ -58,6 +159,8 @@ def calculation(request):
                 vals.remove(vals[i+1])
                 opr.remove(opr[i])
                 vals[i]=str(res)
+                # print(vals)
+                # print(opr)
                 
         for o in opr:
             if o=='x':
@@ -66,6 +169,8 @@ def calculation(request):
                 vals.remove(vals[i+1])
                 opr.remove(opr[i])
                 vals[i]=str(res)
+                # print(vals)
+                # print(opr)
                 
         for o in opr:
             if o=='+':
@@ -74,6 +179,8 @@ def calculation(request):
                 vals.remove(vals[i+1])
                 opr.remove(opr[i])
                 vals[i]=str(res)
+                # print(vals)
+                # print(opr)
 
             if o=='-':
                 i=opr.index(o)
@@ -81,7 +188,13 @@ def calculation(request):
                 vals.remove(vals[i+1])
                 opr.remove(opr[i])
                 vals[i]=str(res)
+                # print(vals)
+                # print(opr)
 
+        # print(vals)
+        # print(opr)
+        # print(res)
+        
         if len(opr)!=0:
             if opr[0]=='÷':
                 result = float(vals[0])/float(vals[1])
